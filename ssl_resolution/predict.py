4644f30063e8c33257d5a72835a659657b68cd3c
Shijun Cheng <112085870+sunnyshijuncheng@users.noreply.github.com>
2025-04-13 15:50:45 +0300

    Update predict.py

--- ssl_resolution/predict.py
+++ ssl_resolution/predict.py
@@ -10,11 +10,9 @@ from train import load_config
 
 args = load_config('config.yaml')
 
-dir_input='./test/input/'
 dir_output='./test/output/'
 dir_cp = './checkpoints/CP_epoch'
 
-os.makedirs(dir_input, exist_ok=True)
 os.makedirs(dir_output, exist_ok=True)
 
 test_ids = [os.path.splitext(file)[0] for file in os.listdir(args.dir_test) 
@@ -30,13 +28,13 @@ for ip, cp in enumerate(args.cp_list):
     # Load corresponding model architecture and weights
     net.load_state_dict(torch.load(f'{dir_cp}{cp}.pth', map_location=device))
 
-    os.makedirs(dir_output + 'epoch', exist_ok=True)
+    os.makedirs(f'{dir_output}epoch{cp}', exist_ok=True)
 
     for i, fn in enumerate(test_ids):
         tar_file = os.path.join(args.dir_test, fn)
         print("\nPredicting seismic data {} ...".format(fn))
         dict = sio.loadmat(tar_file)
-        tar_img = dict['label']
+        tar_img = dict['input']
         tar_img = torch.from_numpy(tar_img.copy()).unsqueeze(0).unsqueeze(1).type(torch.FloatTensor).cuda()
 
         with torch.no_grad():
@@ -44,7 +42,7 @@ for ip, cp in enumerate(args.cp_list):
 
         pred = pred.cpu().squeeze().numpy()
 
-        sio.savemat(dir_output + 'epoch' + str(cp) + '/' + fn + '_out.mat', {'pred': pred})
+        sio.savemat(f'{dir_output}epoch{cp}/{fn}_out.mat', {'pred': pred})
 
         print("\nPredicting seismic data {} have done...".format(fn))
 
